9447e20faad66131565f11a0269aef551d2baa5c
Michelle <mickr592@student.liu.se>
2021-02-10 17:42:44 +0100

    fixup! Fixed eslint errors

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import { test, expect } from 'jest'
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
 })
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-//import './styles/main.scss'
-import { App } from './components/index';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { createMuiTheme, ThemeProvider, StylesProvider} from '@material-ui/core';
-import green from '@material-ui/core/colors/green';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// import './styles/main.scss'
+import { App } from './components/index'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import green from '@material-ui/core/colors/green'
 
 const theme = createMuiTheme({
   palette: {
     primary: green,
     secondary: {
-      main: '#f48fb1',
-    },
+      main: '#f48fb1'
+    }
   },
   props: {
     // Name of the component ⚛️
     MuiButtonBase: {
       // The properties to apply
-      disableRipple: true, // No more ripple, on the whole application 💣!
-    },
-  },
-});
+      disableRipple: true // No more ripple, on the whole application 💣!
+    }
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -33,9 +33,9 @@ ReactDOM.render(
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
